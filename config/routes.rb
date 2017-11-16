1e5f33048572451fa7e7de5bcfd55e5555257dee
Bruno Ukita <brunoukita@gmail.com>
2017-11-16 03:20:33 -0200

    adiciona produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :projects
   resources :supplies
   resources :student_appointments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116043825) do
+ActiveRecord::Schema.define(version: 20171116050443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,15 @@ ActiveRecord::Schema.define(version: 20171116043825) do
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_partners_on_city_id"
     t.index ["email"], name: "index_partners_on_email", unique: true
+  end
+
+  create_table "products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.text "description", default: "", null: false
+    t.float "value", null: false
+    t.string "stock", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
