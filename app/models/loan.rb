3bf84f15991ce861ea302b97fab7e2c5d6ca5548
Johnatan <john.acdc@hotmail.com>
2017-11-26 15:03:18 -0200

    Criada restrição para escolher apenas o aluno ou funcionário no empréstimo; Criada validação se nem o aluno nem funcionário foram selecionados no empréstimo; Craida validação para não permitir salvar o empréstimo com data de retorno anterior à data do empréstimo.

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -7,4 +7,18 @@ class Loan < ApplicationRecord
 
   validates :student_id, presence: false, allow_nil: true
   validates :user_id, presence: false, allow_nil: true
+  validate :valid_return_date
+  validate :student_or_user_selected
+
+  def valid_return_date
+    if return_date < loan_date
+      errors.add(:return_date, 'não pode ser anterior à data do empréstimo!')
+    end
+  end
+
+  def student_or_user_selected
+    unless student || user
+      errors.add(:base, 'Selecione um aluno ou funcionário!')
+    end
+  end
 end
