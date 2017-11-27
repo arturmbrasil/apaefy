3cf7f1729e07f5b0809f9d58684be28caa3c7348
Bruno Ukita <brunoukita@gmail.com>
2017-11-27 14:25:48 -0200

    permissão do Fisioterapeuta

--- app/controllers/student_appointments_controller.rb
+++ app/controllers/student_appointments_controller.rb
@@ -42,7 +42,7 @@ class StudentAppointmentsController < ApplicationController
   end
 
   def permit_user
-    permited_roles = ['director', 'pedagogical_coordinator', 'cr_coordinator', 'psychologist', 'therapist']
+    permited_roles = ['director', 'pedagogical_coordinator', 'cr_coordinator', 'psychologist', 'therapist', 'physiotherapist']
 
     unless permited_roles.include? current_user.role
       redirect_to root_path
