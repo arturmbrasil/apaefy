f831a49b047537ed98ce6edccb3ea59aec4d24b3
Bruno Ukita <brunoukita@gmail.com>
2017-11-30 16:30:04 -0200

    remove mask de dinheiro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -133,8 +133,6 @@ $(function () {
   $('.js-phone-number').mask(phoneMaskBehavior, phoneMaskOptions);
   $('.js-address-zip-code').mask('00000-000');
   $('.js-license-plate').mask('AAA-0000');
-  $('.js-currency').mask('000.000.000.000.000,00', { reverse: true });
-
 
   // Remote forms helpers
   $.fn.renderFormErrors = function(errors) {
